69bb3374c6884c9bd2f7439a65840980b3b9f3d2
dzheine <simona.kilinskaite@gmail.com>
2022-02-21 20:16:55 +0200

    init

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MediLab</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body>
+    <div class="container1">
+          <div class="navbar">
+                <div><img src="dist/img/logo.svg" alt="logo" width="50" height="50"></div>
+                <div>
+                    <ul>
+                        <li>HOME</li>
+                        <li>SERVICES</li>
+                        <li>ABOUT</li>
+                        <li>TESTIMONIALS</li>
+                        <li>CONTACTS</li>
+                    </ul>
+                </div>
+            </div>
+            <DIV class="center">
+            <img src="dist/img/logo.svg" alt="logo" width="100" height="100">
+                <h2>HEALTCHCARE AT YOUR DESK</h2>
+                <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nihil eveniet corporis labore. Nam possimus architecto aliquam, quam tempora et.</P>
+                <div class="app"><a href="#">Make an appointment</a></div>
+                <i class="fa-solid fa-chevron-down"></i>
+            </DIV>
+    </div>
+    <div class="container2">
+        <div class="col1">
+            <h3>OUR SERVICE</h3>
+            <div class="span"></div>
+            <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, esse aspernatur magni voluptas officiis nam porro vitae temporibus quaerat ex voluptates </div>
+        </div>
+        <div class="col2">
+            <div class="item">
+            <i class="fa-solid fa-hand-holding-medical"></i>
+            <h3>24 Hour Support</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quas saepe eveniet.Sit quas saepe eveniet.</p>
+            </div>
+            <div class="item">
+            <i class="fa-solid fa-user-doctor"></i>
+            <h3>Medical Counseling</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quas saepe eveniet.Sit quas saepe eveniet.</p>
+            </div>
+            <div class="item">
+            <i class="fa-solid fa-truck-medical"></i>
+            <h3>Emergency Services</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quas saepe evenietSit quas saepe eveniet..</p>
+            </div>
+            <div class="item">
+            <i class="fa-solid fa-suitcase-medical"></i>
+            <h3>Premium Healthcare</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quas saepe Sit quas saepe eveniet.eveniet.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container3">
+        <div class="case">
+            <h4>Emergency Case</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium nobis debitis sed officia illo. </p>
+        </div>
+        <div class="case">
+            <h4>Emergency Case</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium nobis debitis sed officia illo. </p>
+        </div>
+        <div class="case">
+            <h4>Opening Hours</h4>
+            <div class="hours">
+                <div class="days">
+                    <span>Monday-Friday</span>
+                    <span>Saturday</span>
+                    <span>Sunday</span>
+                </div>
+                <div class="time">
+                    <span>8.00 - 17.00</span>
+                    <span>9.30 - 17.30</span>
+                    <span>9.30 - 15.00</span></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
